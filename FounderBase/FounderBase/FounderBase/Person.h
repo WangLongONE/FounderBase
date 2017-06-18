8c3bea56afc75537651d7d5433288f13571cc106
wangxinglong <wangxinglong@founder.com.cn>
2017-06-18 18:57:09 +0800

    增加person

---
+++ FounderBase/FounderBase/FounderBase/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  FounderBase
+//
+//  Created by mac on 2017/6/18.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ FounderBase/FounderBase/FounderBase/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  FounderBase
+//
+//  Created by mac on 2017/6/18.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
