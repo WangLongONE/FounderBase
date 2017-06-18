e4f36bdb8f632dbe6bd50e1e3f27f255827813c5
wangxinglong <wangxinglong@founder.com.cn>
2017-06-18 20:18:39 +0800

    改nameid为name,增加了age

--- FounderBase/FounderBase/FounderBase/Person.h
+++ FounderBase/FounderBase/FounderBase/Person.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface Person : NSObject
-@property (nonatomic,strong)NSString * nameId;
+@property (nonatomic,strong)NSString * name;
+@property (nonatomic,assign)int age;
 @end
